f84632abe8918d8521b7a0aa50b4cdb80b8351c9
xjyribro <limcheekee.63@gmail.com>
2024-02-27 15:20:22 +0800

    revamped map

--- assets/tiles/apt_front_japanse.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="apt_front_japanse" tilewidth="32" tileheight="32" tilecount="400" columns="20">
- <image source="assets/images/tile_maps/apt_front_japanse.png" width="640" height="640"/>
-</tileset>
---
+++ assets/tiles/buildings.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="buildings" tilewidth="32" tileheight="32" tilecount="3250" columns="65">
+ <image source="assets/images/tile_maps/buildings.png" width="2080" height="1600"/>
+</tileset>
