33efb1e1e8b7bae1a13a41ad671a2d8eacda478c
xjyribro <limcheekee.63@gmail.com>
2024-03-01 14:40:40 +0800

    added player garden sign text

--- lib/game/ui/info_popup/info_popup_data.dart
+++ lib/game/ui/info_popup/info_popup_data.dart
@@ -26,6 +26,7 @@ class InfoPopData {
         InfoPopupObject(text: InfoText.friendship_square);
     _data['park_sign'] = InfoPopupObject(text: InfoText.park_sign);
     _data['school'] = InfoPopupObject(text: InfoText.school);
+    _data['player_garden'] = InfoPopupObject(text: InfoText.school);
     // Park
     _data['zen_garden'] = InfoPopupObject(text: InfoText.zen_garden);
     _data['shrine_complex'] = InfoPopupObject(text: InfoText.shrine_complex);
