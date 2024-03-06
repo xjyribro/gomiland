9e3a4d5818bc4a1d021dc7349cc6fd543217e24b
xjyribro <limcheekee.63@gmail.com>
2024-03-06 15:42:42 +0800

    increased night dim

--- lib/game/ui/hud/brightness.dart
+++ lib/game/ui/hud/brightness.dart
@@ -26,7 +26,7 @@ class BrightnessOverlay extends RectangleComponent
   void makeNightDim() {
     add(
       OpacityEffect.to(
-        0.3,
+        0.4,
         EffectController(
           duration: 1,
           curve: Curves.easeOut,
